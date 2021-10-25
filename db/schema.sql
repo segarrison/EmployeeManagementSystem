52303441fafc965a9e691dd7d5e03ec892787f13
Garrison <79775636+segarrison@users.noreply.github.com>
2021-10-24 23:36:09 -0500

    set up sql files

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS employeemanagementsys_db;
+
+USE employeemanagementsys_db;
+
+CREATE TABLE IF NOT EXISTS department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dep_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(9,2) NOT NULL,
+    dep_id INT,
+    FOREIGN KEY (dep_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT DEFAULT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (dep_name)
+VALUES ("Human Resources"),
+       ("Engineering"),
+       ("IT"),
+       ("Business");
+
+INSERT INTO role (title, salary, dep_id)
+VALUES ("HR Manager", 70000.00, 1),
+       ("HR Team", 50000.00, 1),
+       ("Engineering Manager", 120000.00, 2),
+       ("Engineering Team", 85000,00, 2)
+       ("IT Manager", 100000.00, 3),
+       ("IT Team", 75000.00, 3),
+       ("Business Manager", 150000.00, 4),
+       ("Business Team", 70000.00, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Leia", "Organa", 7,),
+       ("Mon", "Mothma", 8, 1),
+       ("Obi-Wan", "Kenobi", 1,),
+       ("C3", "P0", 2, 3),
+       ("Luke", "Skywalker", 3,),
+       ("Wedge", "Antilles", 4, 5)
+       ("Chewbacca", ,5,),
+       ("R2", "D2", 6, 7 ),
